f99b77995bdf7a2e1a7b3409058cadca0a32ae63
everton_bellmann <everton.bellmann@klab.com.br>
2023-06-05 18:01:12 -0300

    feat: verify-login

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -23,6 +23,20 @@ module.exports = {
         }
 
         return next();
+    },
+
+
+    async loginIn(req, res, next) {
+        const authHeader = req.headers.authorization;
+
+        try {
+            const [, token] = authHeader.split(' ');
+            const decoded = jwt.verify(token, secretToken);
+
+          return res.status(200).json({loginIn: true});
+        } catch (err) {
+            return res.status(401).json({ error: 'Token inválido' });
+        }
     }
 
 
