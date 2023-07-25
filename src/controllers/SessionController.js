a119f9f5b0de46847fe01b691086320259ffca9a
everton_bellmann <everton.bellmann@klab.com.br>
2023-07-25 00:01:54 -0300

    feat:"change token exp "

--- src/controllers/SessionController.js
+++ src/controllers/SessionController.js
@@ -35,7 +35,7 @@ module.exports = {
     return res.json({
         user:{
           id,email,
-        }, token: jwt.sign({id},secretToken,{expiresIn:'1h',},),
+        }, token: jwt.sign({id},secretToken, {expiresIn:"50s",},),
       })
 
 
